8c81ca60c3f58a63acfd9d27dd15a534c0d8381c
RianAdriansyah <rianjh2@gmail.com>
2019-07-06 07:49:45 +0700

    mencoba konek template ke heroku

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/prakerin', function () {
     return view('index');
